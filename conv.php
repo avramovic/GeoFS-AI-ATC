aca7e6b4c93f4e93c4b0d64122c8d714cac660c4
Nemanja <nemanja.avramovic@partech.com>
2025-01-12 01:54:17 +0100

    added airport conversion script

---
+++ conv.php
@@ -0,0 +1,15 @@
+<?php
+$json = json_decode(file_get_contents('csvjson.json'), true);
+$new = [];
+foreach ($json as $ap) {
+	$new[$ap['icao_code']] = [
+    'name' => $ap['airport_name'],
+    'controller' => []
+  ];
+}
+
+$out = json_encode($new);
+file_put_contents('airports.json', $out);
+//$out = json_encode($new, JSON_PRETTY_PRINT);
+//file_put_contents('airports-pretty.json', $out);
+print('done');
